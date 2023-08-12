eacfece06b03ca0c1615b47623a552e6deb3d30b
bytes <therealbyt3s@gmail.com>
2023-08-12 09:41:18 +0200

    feat: clean and merkle proof

--- src/L2GasUsed.sol
+++ src/L2GasUsed.sol
@@ -1,21 +1,18 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import {SecureMerkleTrie} from "./trie/SecureMerkleTrie.sol";
+import {RLPWriter} from "./rlp/RLPWriter.sol";
+
 contract L2GasUsed {
-    // state root => indexer contract storage root
-    mapping(bytes32 => bytes32) public storageRoots;
+    event NewValue(address addr, bytes32 storageRoot, uint256 value);
+
     // storage root => (address => gas used)
     mapping(bytes32 => mapping(address => uint256)) public gasUsed;
     // address => storage root
     mapping(address => bytes32) public lastStorageRoot;
     // storage root => total proven gas
     mapping(bytes32 => uint256) public totalProvenGas;
-
-    event NewValue(address addr, bytes32 storageRoot, uint256 value);
-
-    function getStorageRoot(bytes32 stateRoot) public view returns (bytes32) {
-        return storageRoots[stateRoot];
-    }
 
     function getGasUsed(
         bytes32 storageRoot,
@@ -28,30 +25,14 @@ contract L2GasUsed {
         return lastStorageRoot[addr];
     }
 
-    function getLastValue(address addr) public view returns (uint256) {
-        return getGasUsed(getLastStorageRoot(addr), addr);
-    }
-
     function getTotalProvenGas(
         bytes32 storageRoot
     ) public view returns (uint256) {
         return totalProvenGas[storageRoot];
     }
 
-    function verifyAndSetStorageRoot(
-        bytes32 stateRoot,
-        bytes32 storageRoot,
-        bytes32[] memory proof
-    ) public {
-        if (storageRoots[stateRoot] != 0) {
-            revert("Storage root already set");
-        }
-
-        // Verify the Merkle proof
-        // require(verifyMerkleProof(storageRoot, proof, stateRoot), "Invalid Merkle proof");
-
-        // If proof is valid, set the value in the mapping
-        storageRoots[stateRoot] = storageRoot;
+    function getLastGasUsed(address addr) public view returns (uint256) {
+        return getGasUsed(getLastStorageRoot(addr), addr);
     }
 
     function verifyAndSetGasUsed(
@@ -60,20 +41,28 @@ contract L2GasUsed {
         uint256 value,
         bytes32[] memory proof
     ) public {
-        if (gasUsed[storageRoot][addr] != 0) {
+        if (getGasUsed(storageRoot, addr) > 0) {
             revert("Gas used already set");
         }
 
         bytes32 storageKey = keccak256(abi.encode(addr, 0));
 
         // Verify the Merkle proof
-        // require(verifyMerkleProof(_addr, value, proof, stateRoot), "Invalid Merkle proof");
+        require(
+            SecureMerkleTrie.verifyInclusionProof(
+                abi.encode(storageKey),
+                RLPWriter.writeUint(value),
+                proof,
+                storageRoot
+            ),
+            "Invalid inclusion proof"
+        );
 
-        // If proof is valid, set the value in the mapping
+        // If proof is valid, set the value in the mappings
         gasUsed[storageRoot][addr] = value;
         totalProvenGas[storageRoot] += value;
 
-        if (value > getLastValue(addr)) {
+        if (value > getLastGasUsed(addr)) {
             lastStorageRoot[addr] = storageRoot;
         }
 
